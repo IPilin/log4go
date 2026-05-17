860d16aba4c261b2abfc222278100fd052005160
Alex <lipilinalexey@gmail.com>
2026-05-17 23:20:30 +0300

    Create multi_appender.go

---
+++ multi_appender.go
@@ -0,0 +1,74 @@
+package log4go
+
+import (
+	"errors"
+	"fmt"
+)
+
+type MultiAppender struct {
+	Appenders map[string](Appender)
+}
+
+func NewMultiAppender(v ...*AppenderConfig) (*MultiAppender, error) {
+	ma := &MultiAppender{}
+
+	a := make(map[string](Appender))
+	for _, val := range v {
+		app, err := appenderFromConfig(val)
+		if err != nil {
+			return nil, err
+		}
+		a[app.Key()] = app
+	}
+	ma.Appenders = a
+	return ma, nil
+}
+
+func (m *MultiAppender) Init() error {
+	var errs error
+	for _, a := range m.Appenders {
+		err := a.Init()
+		if err != nil {
+			errs = errors.Join(errs, err)
+		}
+	}
+	return errs
+}
+
+func (m *MultiAppender) Write(r *Record) error {
+	var errs error
+	for _, a := range m.Appenders {
+		err := a.Write(r)
+		if err != nil {
+			errs = errors.Join(errs, err)
+		}
+	}
+	return errs
+}
+
+func (m *MultiAppender) Close() error {
+	var errs error
+	for _, a := range m.Appenders {
+		err := a.Close()
+		if err != nil {
+			errs = errors.Join(errs, err)
+		}
+	}
+	return errs
+}
+
+func appenderFromConfig(ca *AppenderConfig) (Appender, error) {
+	var appender Appender
+	switch ca.Type {
+	case AppenderTypeConsole:
+		appender = &ConsoleAppender{
+			Target: ca.Target,
+			Format: ca.Format,
+		}
+	case AppenderTypeFile:
+	default:
+		return nil, fmt.Errorf("wrong AppenderType: %q", ca.Type)
+	}
+
+	return appender, nil
+}
